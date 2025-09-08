d1dafa58ceaab0ea18acb36d5c3b173a793b3d88
Frederick Arthur <khingarthur01@gmail.com>
2025-09-08 20:50:14 +0000

    User data script

--- terraform/script.sh
+++ terraform/script.sh
@@ -3,7 +3,13 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
-              
-# Pull and run the Docker image from Docker Hub.
+             
+# Add the 'ubuntu' user to the 'docker' group.
 sudo usermod -aG docker ubuntu
+
+# Apply the new group membership to the current shell session.
+# This avoids the need to log out and back in.
+sudo newgrp docker
+
+# Pull and run the Docker image from Docker Hub.
 sudo docker run -d -p 5000:5000 --name hello-app khingarthur/hello-world-app:latest
